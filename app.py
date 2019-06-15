99597fda24c82f006149a433736c8f9d7d8b4923
lozog95 <lozog95@gmail.com>
2019-06-15 18:00:08 +0200

    changed debug to false

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ class Password(Resource):
 api.add_resource(Password, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000')
